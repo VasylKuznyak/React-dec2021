2d218d3196f07f478d743dc42cafd5bdf83c162d
Vasyl Kuznyak <vasyl.kuznyak@gmail.com>
2022-04-05 19:06:56 +0300

    create file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import './App.css';
-
-import {Users} from "./components/Users/Users";
-import {Posts} from "./components/Posts/Posts";
-import {Comments} from "./components/Comments/Comments";
 
 const App = () => {
     return (
         <div>
-            <Users/>
-            <hr/>
-            <Posts/>
-            <hr/>
-            <Comments/>
+
         </div>
     );
 };
