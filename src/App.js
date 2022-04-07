6b70175a3a836f8d355df46ec2c85f25a044852f
Vasyl Kuznyak <vasyl.kuznyak@gmail.com>
2022-04-07 22:20:49 +0300

    create practice file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
-import {useState} from "react";
-
-import './App.css';
-import {UserPost, Users} from "./components";
+import {useEffect, useState} from "react";
+import {userService} from "./services";
 import {postService} from "./services/post.service";
 
 const App = () => {
-    const [onePost, setOnePost] = useState(null);
+    const [users, setUsers] = useState([]);
+    const [posts, setPosts] = useState([]);
 
-    const idForPost = async (id) => {
-        const {data} = await postService.getOne(id);
-        setOnePost(data)
-    }
+    useEffect(() => {
+        userService.getAll().then(({data}) => setUsers(data))
+    }, [])
+
+    useEffect((id) => {
+        postService.getOne(id).then(({data}) => setPosts(data))
+        const filteredPosts = posts.filter(post => users.id === posts.userId);
+    }, [])
+
+
+    // const getUserId = async (id) => {
+    //     const {data} = await userService.getOne(id)
+    //     setUserDetails(data)
+    // }
 
     return (
         <div>
-            <Users idForPost={idForPost}/>
-            {onePost && <UserPost onePost={onePost}/>}
+            <form onSelect={onSelect}>
+                <select>
+                    {users.map((user) => <option value={user.id} key={user.id}>{user.name}</option>)}
+                </select>
+            </form>
         </div>
     );
 };
