49adc7b50f6511f93daafe89a317c8aa87f97baa
Vasyl Kuznyak <vasyl.kuznyak@gmail.com>
2022-04-26 00:01:22 +0300

    create file home work 7

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import {Counter, Form, Users} from "./components";
-
 function App() {
     return (
         <div>
-            <Counter/>
-            <Form/>
-            <Users/>
+
         </div>
     );
 }
--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export * from './Counter';
-export * from './Form';
-export * from './User';
-export * from './Users';
