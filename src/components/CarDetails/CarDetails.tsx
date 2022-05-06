e9f77fd0999902edc176c9e1fc9d83326a366439
Vasyl Kuznyak <vasyl.kuznyak@gmail.com>
2022-05-06 16:51:05 +0300

    done practice ts

--- src/components/CarDetails/CarDetails.tsx
+++ src/components/CarDetails/CarDetails.tsx
@@ -1,30 +1,39 @@
 import {ICar} from "../../interfaces";
 import {FC} from "react";
+import {useNavigate} from "react-router-dom";
 
 import {useAppDispatch} from "../../hook";
 import {carActions} from "../../redux";
+import style from './CarDetails.module.css';
 
 interface IProps {
     car: ICar
 }
 
 const CarDetails: FC<IProps> = ({car}) => {
-    const {id, model, price, year} = car
+    const navigate = useNavigate();
+    const {id, model, price, year} = car;
 
     const dispatch = useAppDispatch();
 
     const deleteById = async () => {
         await dispatch(carActions.deleteByIdThunk({id}));
+        navigate('/cars');
+    };
+
+    const updateById = async () => {
+        await dispatch(carActions.setToUpdate({car}));
+        navigate('/cars');
     };
 
     return (
-        <>
+        <div className={style.carDetails}>
             <div>{model}</div>
             <div>{price}</div>
             <div>{year}</div>
             <button onClick={deleteById}>Delete</button>
-            <button>Update</button>
-        </>
+            <button onClick={updateById}>Update</button>
+        </div>
     );
 };
 
--- src/components/Cars/Cars.tsx
+++ src/components/Cars/Cars.tsx
@@ -2,6 +2,7 @@ import {FC} from "react";
 import {Link} from "react-router-dom";
 
 import {ICar} from "../../interfaces";
+import style from './Style.module.css'
 
 interface IProps {
     car: ICar
@@ -10,12 +11,12 @@ interface IProps {
 const Cars: FC<IProps> = ({car, car: {id, model}}) => {
 
     return (
-        <>
-            <div>{model}</div>
+        <div className={style.singleCar}>
+            <h3>{model}</h3>
             <Link to={id.toString()} state={car}>
                 <button>Details</button>
             </Link>
-        </>
+        </div>
     );
 };
 
